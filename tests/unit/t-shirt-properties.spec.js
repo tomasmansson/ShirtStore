79ed905b1723fe713e14072685c85202721e085b
tomasmansson <tomas.mansson93@gmail.com>
2019-10-01 14:58:33 +0200

    suggestion test t-shirt config

--- tests/unit/t-shirt-properties.spec.js
+++ tests/unit/t-shirt-properties.spec.js
@@ -4,10 +4,16 @@
 // import Shop from '@/components/Shop.vue'
 
 describe('Shop.vue', () => {
-  test('choose size 52', () => {
+  test('choose size: 52', () => {
     const wrapper = mount(t-shirt-config)
     wrapper.chooseSize('52')
     expect(wrapper.vm.tShirt()).toContain('52')
+
+  })
+  test('choose colour: green', () => {
+    const wrapper = mount(t-shirt-config)
+    wrapper.chooseColour('green')
+    expect(wrapper.vm.tShirt()).toContain('green')
 
   })
 })
