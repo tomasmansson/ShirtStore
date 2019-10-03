ac2089439410b8791179cf781d2ddaa1574ed623
tomasmansson <tomas.mansson93@gmail.com>
2019-10-03 09:05:26 +0200

    blue balls

--- tests/unit/t-shirt-properties.spec.js
+++ tests/unit/t-shirt-properties.spec.js
@@ -5,15 +5,22 @@
 
 describe('Shop.vue', () => {
   test('choose size: 52', () => {
-    const wrapper = mount(t-shirt-config)
+    const wrapper = mount(t - shirt - config)
     wrapper.chooseSize('52')
     expect(wrapper.vm.tShirt()).toContain('52')
 
   })
   test('choose colour: green', () => {
-    const wrapper = mount(t-shirt-config)
+    const wrapper = mount(t - shirt - config)
     wrapper.chooseColour('green')
     expect(wrapper.vm.tShirt()).toContain('green')
 
   })
+
+  test('add text: blue balls', () => {
+    const wrapper = mount(t - shirt - config)
+    wrapper.addText('blue balls')
+    expect(wrapper.vm.tShirt()).toContain('blue balls')
+  })
+
 })
