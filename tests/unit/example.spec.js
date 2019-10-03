6397b61596767ff89096784272ee7df7f1cf40c9
tomasmansson <tomas.mansson93@gmail.com>
2019-10-03 08:47:35 +0200

    Thomas tester receptia spec.js

--- tests/unit/example.spec.js
+++ tests/unit/example.spec.js
@@ -1,12 +1,67 @@
-import { shallowMount } from '@vue/test-utils'
-import HelloWorld from '@/components/HelloWorld.vue'
+// import goes here
+// detta är exempel som Thomas gick igenom för receptia-routed-page
+import { mount } from '@vue/test-utils'
+import App from '@/App.vue'
+import Page from '@/views/Page.vue'
+import Home from '@/views/Home.vue'
+import Cart from '@/views/Cart.vue'
 
-describe('HelloWorld.vue', () => {
-  it('renders props.msg when passed', () => {
-    const msg = 'new message'
-    const wrapper = shallowMount(HelloWorld, {
-      propsData: { msg }
-    })
-    expect(wrapper.text()).toMatch(msg)
+
+describe('App', () => {
+  test('is a Vue instance', () => {
+    const wrapper = mount(App)
+    expect(wrapper.isVueInstance()).toBeTruthy()
+  })
+  test('renders template', () => {
+    const wrapper = mount(App)
+    expect(wrapper.text()).toMatch('RECEPTIA')
+  })
+})
+
+describe('Page', () => {
+  test('is a Vue instance', () => {
+    const wrapper = mount(Page)
+    expect(wrapper.isVueInstance()).toBeTruthy()
+  })
+  test('data contains categories', () => {
+    const wrapper = mount(Page)
+    expect(wrapper.vm.categories).toBeDefined()
+  })
+})
+
+describe('Cart', () => {
+  test('is a Vue instance', () => {
+    const wrapper = mount(Cart)
+    expect(wrapper.isVueInstance()).toBeTruthy()
+  })
+  test('cart must have a contents array', () => {
+    const wrapper = mount(Cart)
+    expect(wrapper.vm.contents).toBeDefined()
+  })
+
+  test('add burger to cart', () => {
+    const wrapper = mount(Cart)
+    wrapper.vm.addToCart('burger')
+    expect(wrapper.vm.contents).toContain('burger')
+  })
+  test('remove burger from cart', () => {
+    const wrapper = mount(Cart)
+    wrapper.vm.addToCart('burger')
+    wrapper.vm.removeFromCart('burger')
+    expect(wrapper.vm.contents).not.toContain('burger')
+  })
+  test('place succesful order', () => {
+    const wrapper = mount(Cart)
+    wrapper.vm.addToCart('burger')
+    wrapper.vm.addToCart('shake')
+    wrapper.vm.addToCart('fries')
+    wrapper.vm.placeOrder()
+    expect(wrapper.vm.contents).toHaveLength(0)
+    expect(wrapper.text()).toContain('Your order will be done in 5 minutes')
+  })
+  test('fail to place empty order', () => {
+    const wrapper = mount(Cart)
+    wrapper.vm.placeOrder()
+    expect(wrapper.text()).not.toContain('Your order will be done in 5 minutes')
   })
 })
