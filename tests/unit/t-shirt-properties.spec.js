7b9e446af347d584d190a2c0d8bf23266d97df0a
tomasmansson <tomas.mansson93@gmail.com>
2019-10-03 09:16:45 +0200

    tShirtConfig

--- tests/unit/t-shirt-properties.spec.js
+++ tests/unit/t-shirt-properties.spec.js
@@ -5,20 +5,20 @@
 
 describe('Shop.vue', () => {
   test('choose size: 52', () => {
-    const wrapper = mount(t - shirt - config)
+    const wrapper = mount(tShirtConfig)
     wrapper.chooseSize('52')
     expect(wrapper.vm.tShirt()).toContain('52')
 
   })
   test('choose colour: green', () => {
-    const wrapper = mount(t - shirt - config)
+    const wrapper = mount(tShirtConfig)
     wrapper.chooseColour('green')
     expect(wrapper.vm.tShirt()).toContain('green')
 
   })
 
   test('add text: blue balls', () => {
-    const wrapper = mount(t - shirt - config)
+    const wrapper = mount(tShirtConfig)
     wrapper.addText('blue balls')
     expect(wrapper.vm.tShirt()).toContain('blue balls')
   })
