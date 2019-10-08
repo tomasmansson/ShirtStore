b61fa9cad65e5b3bf5c3f171abe2afa868e8da8e
Dan Olsen <dan.olsen85@gmail.com>
2019-10-08 08:53:45 +0200

    bennys components

---
+++ src/components/Order.vue
@@ -0,0 +1,18 @@
+<script>
+export default{
+  data(){
+    return{
+      orderData:{
+        firstName:'',
+        tShirt:{}
+      },
+      setFirstName(name){
+        this.orderData.firstName = name;
+      },
+      setTShirt(tShirt){
+        this.orderData.tShirt = tShirt;
+      }
+    }
+  }
+}
+</script>
---
+++ src/components/TShirtConfig.vue
@@ -0,0 +1,14 @@
+<script>
+export default {
+  data(){
+    return {
+      tShirt: {
+        size: ''
+      },
+      setSize(size){
+        this.tShirt.size = size;
+      }
+    }
+  }
+}
+</script>
