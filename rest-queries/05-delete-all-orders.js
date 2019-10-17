efff084cb46c920a2ba9e0d49abb599f15c6884f
tomasmansson <tomas.mansson93@gmail.com>
2019-10-17 16:02:56 +0200

    cleaning code

--- rest-queries/05-delete-all-orders.js
+++ rest-queries/05-delete-all-orders.js
@@ -1,9 +1,7 @@
-// Note: Using expect test syntax
 module.exports = ({ expect, response }) => ({
   path: 'orders',
   method: 'delete',
   test() {
-    // Expect the deletion to go through without warnings
     expect(response.warningCount).to.equal(0);
   }
 });
--- rest-queries/06-check-no-orders.js
+++ rest-queries/06-check-no-orders.js
@@ -1,13 +1,10 @@
-// Note: Using assert test syntax
 module.exports = ({ assert, response, store }) => ({
   path: 'orders',
   method: 'get',
   test() {
-   // Check that there are no orders in DB
    assert.deepEqual(response, []);
   },
   setup() {
-    // Store the mock orders in the db (setup for next step/query)
     store.mockOrders = require('./mock-orders.json');
   }
 });
