f10c0036ecb44108c2e993ab3cff82f372315c94
tomasmansson <tomas.mansson93@gmail.com>
2019-10-19 09:32:01 +0200

    added comments

--- rest-queries/03-create-users.js
+++ rest-queries/03-create-users.js
@@ -3,8 +3,12 @@ module.exports = ({ response, store, repeat, i }) => ({
   method: 'post',
   body: store.mockUsers[i],
   test() {
+    // check that a row was inserted in the db
     response.affectedRows.should.equal(1);
+    // store the inserted id in mockUsers
     store.mockUsers[i].userid = response.insertId;
+    // repeat this step/query as long as
+    // there a more mockUsers to insert
     if(store.mockUsers[i + 1]){
       repeat();
     }
