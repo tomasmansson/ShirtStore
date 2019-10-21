01b24ddd47c2ab312888778630f1f41d6d4a1174
tomasmansson <tomas.mansson93@gmail.com>
2019-10-21 11:54:54 +0200

    removed comments

--- rest-queries/04-check-all-users-inserted.js
+++ rest-queries/04-check-all-users-inserted.js
@@ -1,12 +1,8 @@
-// Note: Using assert test syntax
 module.exports = ({ assert, response, store }) => ({
   path: 'users',
   method: 'get',
   test() {
-    // check that we got the right number of users
     assert.equal(response.length, store.mockUsers.length);
-    // check that all users where
-    // inserted correctly with correct data
     assert.deepEqual(response, store.mockUsers);
   }
 });
