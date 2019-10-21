bae984bb90aa255dca9e01ca18d08baacc23f40f
tomasmansson <tomas.mansson93@gmail.com>
2019-10-22 00:28:46 +0200

    new .sql export

--- Shirtstore_individuell.sql
+++ Shirtstore_individuell.sql
@@ -24,10 +24,20 @@ CREATE TABLE IF NOT EXISTS `orders` (
   `discount` int(11) DEFAULT '0',
   `delivered` tinyint(4) NOT NULL DEFAULT '0',
   PRIMARY KEY (`orderid`)
-) ENGINE=InnoDB AUTO_INCREMENT=912 DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB AUTO_INCREMENT=1500 DEFAULT CHARSET=utf8mb4;
 
 -- Dumpar data för tabell Shirtstore.orders: ~9 rows (ungefär)
 /*!40000 ALTER TABLE `orders` DISABLE KEYS */;
+INSERT IGNORE INTO `orders` (`orderid`, `userid`, `price`, `discount`, `delivered`) VALUES
+	(1491, 1595, 100, 0, 1),
+	(1492, 1596, 100, 10, 1),
+	(1493, 1597, 100, 0, 1),
+	(1494, 1598, 100, 0, 1),
+	(1495, 1599, 100, 10, 0),
+	(1496, 1600, 100, 10, 0),
+	(1497, 1601, 100, 0, 0),
+	(1498, 1602, 100, 10, 0),
+	(1499, 1603, 100, 10, 0);
 /*!40000 ALTER TABLE `orders` ENABLE KEYS */;
 
 -- Dumpar struktur för vy Shirtstore.otherstshirts
@@ -67,20 +77,20 @@ CREATE TABLE IF NOT EXISTS `tshirts` (
   `imgsize` int(11) NOT NULL,
   `sharable` tinyint(4) NOT NULL,
   PRIMARY KEY (`tshirtid`)
-) ENGINE=InnoDB AUTO_INCREMENT=344 DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB AUTO_INCREMENT=650 DEFAULT CHARSET=utf8mb4;
 
 -- Dumpar data för tabell Shirtstore.tshirts: ~9 rows (ungefär)
 /*!40000 ALTER TABLE `tshirts` DISABLE KEYS */;
 INSERT IGNORE INTO `tshirts` (`tshirtid`, `userid`, `orderid`, `text`, `textsize`, `colour`, `material`, `size`, `model`, `textcolour`, `bold`, `cursive`, `font`, `img`, `imgsize`, `sharable`) VALUES
-	(335, 740, 903, 'jaja', 13, 'white', 'cotton', 'M', '1', 'blue', 1, 1, 'calibri', '', 0, 0),
-	(336, 741, 904, 'pewpew', 12, 'black', 'cotton', 'L', '0', 'green', 0, 0, 'helvetica', '', 0, 0),
-	(337, 742, 905, 'boss', 18, 'blue', 'cotton', 'XXL', '5', 'blue', 0, 0, 'arial', '', 0, 0),
-	(338, 743, 906, 'kung i baren', 72, 'white', 'cotton', 'S', '3', 'grey', 0, 0, 'verdana', '', 0, 0),
-	(339, 744, 907, 'l.messi', 22, 'red', 'cotton', 'XL', '3', 'black', 0, 0, 'helvetica', '', 0, 0),
-	(340, 745, 908, 'superzlatan', 32, 'yellow', 'cotton', 'M', '4', 'white', 1, 0, 'arial', '', 0, 0),
-	(341, 746, 909, 'davve', 17, 'black', 'cotton', 'M', '1', 'blue', 1, 1, 'calibri', '', 0, 0),
-	(342, 747, 910, 'pauw', 13, 'grey', 'cotton', 'XL', '2', 'white', 1, 0, 'verdana', '', 0, 0),
-	(343, 748, 911, 'iffiff', 22, 'lightblue', 'cotton', 'XS', '0', 'white', 0, 1, 'cambria', '', 0, 1);
+	(641, 1595, 1491, 'jaja', 13, 'white', 'cotton', 'M', '1', 'blue', 1, 1, 'calibri', '', 0, 0),
+	(642, 1596, 1492, 'pewpew', 12, 'black', 'cotton', 'L', '0', 'green', 0, 0, 'helvetica', '', 0, 1),
+	(643, 1597, 1493, 'boss', 18, 'blue', 'cotton', 'XXL', '5', 'blue', 0, 0, 'arial', '', 0, 0),
+	(644, 1598, 1494, 'kung i baren', 72, 'white', 'cotton', 'S', '3', 'grey', 0, 0, 'verdana', '', 0, 1),
+	(645, 1599, 1495, 'l.messi', 22, 'red', 'cotton', 'XL', '3', 'black', 0, 0, 'helvetica', '', 0, 0),
+	(646, 1600, 1496, 'superzlatan', 32, 'yellow', 'cotton', 'M', '4', 'white', 1, 0, 'arial', '', 0, 0),
+	(647, 1601, 1497, 'davve', 17, 'black', 'cotton', 'M', '1', 'blue', 1, 1, 'calibri', '', 0, 0),
+	(648, 1602, 1498, 'pauw', 13, 'grey', 'cotton', 'XL', '2', 'white', 1, 0, 'verdana', '', 0, 0),
+	(649, 1603, 1499, 'iffiff', 22, 'lightblue', 'cotton', 'XS', '0', 'white', 0, 1, 'cambria', '', 0, 1);
 /*!40000 ALTER TABLE `tshirts` ENABLE KEYS */;
 
 -- Dumpar struktur för tabell Shirtstore.users
@@ -94,20 +104,20 @@ CREATE TABLE IF NOT EXISTS `users` (
   `postnr` int(11) NOT NULL DEFAULT '0',
   `city` char(50) NOT NULL DEFAULT '0',
   PRIMARY KEY (`userid`)
-) ENGINE=InnoDB AUTO_INCREMENT=767 DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB AUTO_INCREMENT=1604 DEFAULT CHARSET=utf8mb4;
 
 -- Dumpar data för tabell Shirtstore.users: ~9 rows (ungefär)
 /*!40000 ALTER TABLE `users` DISABLE KEYS */;
 INSERT IGNORE INTO `users` (`userid`, `username`, `password`, `firstname`, `lastname`, `address`, `postnr`, `city`) VALUES
-	(758, 'Rambo', 'trughjjbvcbf', 'sylvester', 'stallone', 'roundhouse 2', 28133, 'monstercity'),
-	(759, 'mongo', 'xvcnxcvmn4567', 'jimmy', 'mccartney', 'byavaejen', 67567, 'glimmen'),
-	(760, 'bongo', '785485=)//', 'kalle', 'anka', 'västra hamngatan 23', 55852, 'ankeborg'),
-	(761, 'qwerty', 'djkhgsldkjfg', 'frans', 'jansson', 'vägen', 23435, 'by23'),
-	(762, 'kungen', 'ajsfdjkbb767.', 'daddy', 'DJ', 'partyvägen', 65748, 'lomma'),
-	(763, 'baghera', 'hakkahukka', 'leijon', 'mannen', 'torget', 34564, 'norje'),
-	(764, 'willys', '564263647', 'willy', 'wonka', 'jajjaveijen', 89899, 'eldorado'),
-	(765, 'hercules', 'hjsweyusdhj', 'donald', 'trumpis', 'kyrkvägen', 54857, 'gotham'),
-	(766, 'megaman', 'kjhsndhey&/&/', 'john', 'johnson', '1st street', 85458, 'hököpinge');
+	(1595, 'Rambo', 'trughjjbvcbf', 'sylvester', 'stallone', 'roundhouse 2', 28133, 'monstercity'),
+	(1596, 'mongo', 'xvcnxcvmn4567', 'jimmy', 'mccartney', 'byavaejen', 67567, 'glimmen'),
+	(1597, 'bongo', '785485=)//', 'kalle', 'anka', 'västra hamngatan 23', 55852, 'ankeborg'),
+	(1598, 'qwerty', 'djkhgsldkjfg', 'frans', 'jansson', 'vägen', 23435, 'by23'),
+	(1599, 'kungen', 'ajsfdjkbb767.', 'daddy', 'DJ', 'partyvägen', 65748, 'lomma'),
+	(1600, 'baghera', 'hakkahukka', 'leijon', 'mannen', 'torget', 34564, 'norje'),
+	(1601, 'willys', '564263647', 'willy', 'wonka', 'jajjaveijen', 89899, 'eldorado'),
+	(1602, 'hercules', 'hjsweyusdhj', 'donald', 'trumpis', 'kyrkvägen', 54857, 'gotham'),
+	(1603, 'megaman', 'kjhsndhey&/&/', 'john', 'johnson', '1st street', 85458, 'hököpinge');
 /*!40000 ALTER TABLE `users` ENABLE KEYS */;
 
 -- Dumpar struktur för vy Shirtstore.otherstshirts
